a10ff59b27a0ec70c0e7717aa8426c2cf96661a4
unknown <02230064.cst@rub.edu.bt>
2024-02-29 06:48:27 -0800

    i like it

--- sum.py
+++ sum.py
@@ -6,5 +6,4 @@ nam2 = float(input("Enter your second number:\n"))
 subtract_result = nam1-nam2
 addition_result = nam1+nam2
 
-print(f"Hello! the sum of two number is: {addition_result} and the difference of two number is: {subtract_result}.")
-#print("The result is:" substraction_result)
+print(f"Hello!{name} the sum of two number is: {addition_result} and the difference of two number is: {subtract_result}.")
