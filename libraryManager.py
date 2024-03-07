671a7cf471c70de402f165fe69ebbfcd3e842c4b
unknown <02230064.cst@rub.edu.bt>
2024-03-07 11:28:09 -0800

    library manager

---
+++ libraryManager.py
@@ -0,0 +1,50 @@
+#initialize empty list[], set(), and dictionary{}
+books_list = []
+authors_set = set()
+books_dict = {}
+
+# add book and its author
+books_list.append("Python Programming")
+authors_set.add("John Smith")
+books_dict["Python Programming"] = "John Smith"
+
+books_list.append("Data Structure and Alogrithm")
+authors_set.add("John Doe")
+books_dict["Data Structre and Alogrithm"] = "John Deo"
+
+books_list.append("Machine learnig basic")
+authors_set.add("Alice Johnson")
+books_dict["Machine learning basic"] = "Alice Johnson"
+
+books_list.append("History of Bhutan")
+authors_set.add("Jigme Namgyel")
+books_dict["History of Bhutan"] = "Jigme Namgyel"
+
+#User input to search book
+search_title = input("Hello la! Enter the tile of the book you wanted:")
+if search_title in books_list:
+    print(f"Book has been founded! Author is {books_dict[search_title]}")
+
+else:
+    print("Sorry la! book not found")
+
+# to display all books
+print("List of books available are:")
+for book in books_list:
+    print(book)
+
+# to remove book
+remove_tilte= input("Enter the name of book you wanted to remove:")
+if remove_tilte in books_list:
+    remove_author = books_dict[remove_tilte]
+    books_list.remove(remove_tilte)
+    authors_set.remove(remove_author)
+    del books_dict[remove_tilte]
+    print("Book removed successfully!")
+    print('books available', books_list)
+
+else:
+    print("Book not founed")
+
+
+
--- logical.py
+++ logical.py
@@ -5,6 +5,12 @@ print( x>19 and x<10 )
 
 # or operater
 
-# y = 5
-# print(y = 5 or y < 10)
-# print(y )
+y = 5
+print(y < 10 or y % 2== 1)
+print(y <3 or y >10 )
+
+# Not operater
+
+z=10
+print(not(z<1 and z>5))
+print(not(z>1 and z % 2 == 2 ))
