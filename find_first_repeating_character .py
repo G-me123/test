7c504e363210bd2ba53a9352c239a45427eab5f1
unknown <02230064.cst@rub.edu.bt>
2024-05-11 04:38:55 -0700

    coding is fun

--- find_first_repeating_character .py
+++ find_first_repeating_character .py
@@ -1,18 +1,8 @@
-def find_first_repeating_character(s):
-    char_count = {}
-    for char in s:
-        if char in char_count:
-            print(f"First repeating character: '{char}' at memory address: {id(char)}")
-            return char, id(char)
+def find_first_repeating_character(j):
+    count = {}
+    for char in j:
+        if char in count:
+            return char, id(char) # print(f"{char} "and" id{char}")
         else:
-            char_count[char] = 1
+            count[char] = 1  #like {count.add(char)} but it is for set only...set only store unique vale not duplicate
     return None
-
-# Example usage:
-result = find_first_repeating_character("hello world")
-if result:
-    character, address = result
-    print(f"The character '{character}' repeats first and is located at memory address {address}.")
-else:
-    print("No repeating characters found.")
-
