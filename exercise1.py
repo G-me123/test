b875c5444c50d3e7bc7954a9b910511b3563620f
unknown <02230064.cst@rub.edu.bt>
2024-05-15 01:43:29 -0700

    exercise student

--- exercise1.py
+++ exercise1.py
@@ -4,7 +4,6 @@ b=input(" Are you student? (yes or no)\n").lower()
 if b == "yes":
     if (a <= 12) or (a<=18 and a>=13):
         print("You will get discount for your movie ticket")
-
     else:
         print("sorry la! your age exceed the discount citeria..so we couldn't help you with the discount la")
 
